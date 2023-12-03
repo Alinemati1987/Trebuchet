25a884f9b6775c76e14bae325e3d2d9b8987191c
Alinemati1987 <89942725+Alinemati1987@users.noreply.github.com>
2023-12-03 11:37:40 +0100

    Function for Part two is added.

--- index.js
+++ index.js
@@ -3,12 +3,8 @@ const fs = require("fs");
 // Main //
 function main() {
   const data = getData();
-  partOne(data);
-
-  //   const sumPortions = iterateData(data);
-  //   const partOne = getResult(1, sumPortions); // Arguments => first: number of items , second: Sum of each snack
-  //   const partTwo = getResult(3, sumPortions); // Arguments => first: number of items , second: Sum of each snack
-  //   consoleResults([partOne, partTwo]);
+  //   partOne(data);
+  partTwo(data);
 }
 
 // Functions //
@@ -22,11 +18,64 @@ function partOne(inputData) {
   consoleResults("Part ONE", sums);
 }
 
+function partTwo(inputData) {
+  let secondSums = 0;
+  let letterDigits = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  let testInput = [
+    "ttwone4vgtsrcds",
+    "oneeighteight8nsnzphnmspkjxzdxhvhkgrl",
+    "2eight2kqmmbsbjvxtvjhponesixtwonesn",
+    "ssoneightfbfctjqv43psixsevenslqsfpkb1",
+    "cvtwone2k1zmp65",
+    "threethreeptz4",
+    "47twothreethreeonethree7",
+    "47twothreeightwonethree7",
+    "kptrrdzxcninesevenfiveeight458",
+    "kptrrdzxcninesevenfiveight458",
+    "43fiveprxftkhpszrgsevenninebncn",
+    "43fiveprxftkhpszrgseveninebncn",
+    "fzqeighthree1qhjtmfdsmsf",
+    "four9threefiveeight2eightgcm",
+    "22threepdtqbceightninesevenvrsct",
+    "22threepdtqbceightnineightsevenvrsct",
+  ];
+
+  inputData.forEach((line) => {
+    // testInput.forEach((line) => {
+    let calDigits = iterateLine(line, letterDigits);
+    secondSums += parseInt(calDigits);
+  });
+  consoleResults("Part TWO", secondSums);
+}
+
 function consoleResults(part, answer) {
   console.log("Answer for " + part + " is: " + answer);
 }
 
-function iterateLine(thisLine) {
+function iterateLine(thisLine, letters) {
+  thisLine.toLowerCase();
+  const checkLetters = new RegExp(
+    /one|two|three|four|five|six|seven|eight|nine/g
+  );
+  const availableLetters = thisLine.match(checkLetters);
+
+  if (availableLetters) {
+    availableLetters.forEach((al) => {
+      thisLine = thisLine.replace(al, letters[al]);
+    });
+  }
+
   let lineDigits = thisLine.match(/\d/g);
   let calNum = lineDigits[0] + lineDigits[lineDigits.length - 1];
   return calNum;
