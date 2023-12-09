c3c35bf4b50e7efb438e07340f53016a3704b976
Alinemati1987 <89942725+Alinemati1987@users.noreply.github.com>
2023-12-09 22:20:36 +0100

    Part two is completed.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 // Main //
 function main() {
   const data = getData();
-  //   partOne(data);
+  partOne(data);
   partTwo(data);
 }
 
@@ -21,40 +21,20 @@ function partOne(inputData) {
 function partTwo(inputData) {
   let secondSums = 0;
   let letterDigits = {
-    one: 1,
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9,
+    one: "1",
+    two: "2",
+    three: "3",
+    four: "4",
+    five: "5",
+    six: "6",
+    seven: "7",
+    eight: "8",
+    nine: "9",
   };
 
-  let testInput = [
-    "ttwone4vgtsrcds",
-    "oneeighteight8nsnzphnmspkjxzdxhvhkgrl",
-    "2eight2kqmmbsbjvxtvjhponesixtwonesn",
-    "ssoneightfbfctjqv43psixsevenslqsfpkb1",
-    "cvtwone2k1zmp65",
-    "threethreeptz4",
-    "47twothreethreeonethree7",
-    "47twothreeightwonethree7",
-    "kptrrdzxcninesevenfiveeight458",
-    "kptrrdzxcninesevenfiveight458",
-    "43fiveprxftkhpszrgsevenninebncn",
-    "43fiveprxftkhpszrgseveninebncn",
-    "fzqeighthree1qhjtmfdsmsf",
-    "four9threefiveeight2eightgcm",
-    "22threepdtqbceightninesevenvrsct",
-    "22threepdtqbceightnineightsevenvrsct",
-  ];
-
   inputData.forEach((line) => {
-    // testInput.forEach((line) => {
-    let calDigits = iterateLine(line, letterDigits);
-    secondSums += parseInt(calDigits);
+    let secondCalDigits = getDigitAndLetter(line, letterDigits);
+    secondSums += parseInt(secondCalDigits);
   });
   consoleResults("Part TWO", secondSums);
 }
@@ -63,19 +43,53 @@ function consoleResults(part, answer) {
   console.log("Answer for " + part + " is: " + answer);
 }
 
-function iterateLine(thisLine, letters) {
-  thisLine.toLowerCase();
+function getDigitAndLetter(theLine, letters) {
+  theLine = theLine.toLowerCase();
   const checkLetters = new RegExp(
-    /one|two|three|four|five|six|seven|eight|nine/g
+    /one|two|three|four|five|six|seven|eight|nine/
   );
-  const availableLetters = thisLine.match(checkLetters);
+  // const availableLetters = thisLine.match(checkLetters);
 
-  if (availableLetters) {
-    availableLetters.forEach((al) => {
-      thisLine = thisLine.replace(al, letters[al]);
-    });
+  let findFirst = true;
+  let findSecond = false;
+  let firstDigit = 0;
+  let secondDigit = 0;
+
+  if (findFirst) {
+    let fd = theLine.match(/\d/);
+    firstDigit = fd;
+    let fl = theLine.match(checkLetters);
+    if (theLine.indexOf(fd) != 0 && fl) {
+      if (theLine.indexOf(fl) < theLine.indexOf(fd)) {
+        firstDigit = letters[fl];
+      }
+    }
+    findSecond = !findSecond;
   }
 
+  if (findSecond) {
+    const lineLength = theLine.length;
+    for (let index = 1; index < lineLength; index++) {
+      let subStr = theLine.substring(lineLength - index);
+      let sd = subStr.match(/\d/);
+
+      if (sd) {
+        secondDigit = sd;
+        break;
+      }
+      let sl = subStr.match(checkLetters);
+      if (sl) {
+        secondDigit = letters[sl];
+        break;
+      }
+    }
+  }
+
+  if (firstDigit && secondDigit) return firstDigit + secondDigit;
+  else return firstDigit + firstDigit;
+}
+
+function iterateLine(thisLine) {
   let lineDigits = thisLine.match(/\d/g);
   let calNum = lineDigits[0] + lineDigits[lineDigits.length - 1];
   return calNum;
